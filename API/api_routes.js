9baed9632f8f8671f333c47020b9cfee486a1374
Dzekewong Karlson Fonyuy <karlsedoide@gmail.com>
2020-07-03 05:21:04 -0400

    update: created the api controllers

--- API/api_routes.js
+++ API/api_routes.js
@@ -1,19 +1,91 @@
-const router = require('express').Router()
-const fs = require('fs');
-const path = require('path');
+const router = require("express").Router();
+const express = require('express')
+const fs = require("fs");
+const path = require("path");
+const stream = require('../utils/videoStream');
+let size = 5675;
+let start = 0;
+let end = 2000000;
+let range = undefined;
+let quota  = undefined;
 
-router.get('/video',(req,res) =>{
-    console.log('Response');
+let chunksize = end - start + 1;
 
-    const videoStream = fs.createReadStream(path.join(__dirname , '../','public','videos','Lilly.mp4'));
-   
-    videoStream.on('data',()=>{
-        videoStream.pipe(res)
-    })
-    // console.log(videoStream)
-    
-    
-    // res.send({message: 'hello From the server '});
+
+let videoStream = stream()
+
+router.use(express.urlencoded({
+    extended: false
+}))
+router.use(express.json());
+
+
+router.get("/video", (req, res) => {
+
+    if(quota){
+        console.log('The kwota');
+        console.log(req.headers.range)
+        console.log(quota)
+        console.log('Yeah lets make this happen');
+    }
+    else{
+
+
+    let head = {
+        "Content-Range": "bytes " + start + "-" + end + "/" + size,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(200, head);
+
+
+    videoStream.on("open", function () {
+        console.log("Piping the things");
+        videoStream.pipe(res);
+        console.log("After the pipe");
+    });
+
+    videoStream.on("error", () => {
+        console.log("Some sort of error occued , we're closing");
+        videoStream.close();
+    });
+}
+
 })
 
-module.exports = router
+.post('/video',(req, res)=>{
+    
+    console.log(req.body.quota)
+    quota = +req.body.quota;
+    quota*= 1e6;
+    console.log(quota)
+
+    videoStream = stream(quota)
+    console.log('Filled the stream with a post quota')
+
+    let head = {
+        "Content-Range": "bytes " + start + "-" + end + "/" + size,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(200, head);
+
+    videoStream.on("open", function () {
+        console.log("Posting the things");
+        videoStream.pipe(res);
+    });
+
+    videoStream.on("error", () => {
+        console.log("Some sort of error occued , we're closing");
+        videoStream.close();
+    })
+    // res.render('home',{controls:"controls"})
+
+})
+
+
+module.exports = router;    
